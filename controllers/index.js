1af174debdb3a2858bc580b9b5a4ce68f0c2e1c0
Thomson2412 <thomasm.w.fink@gmail.com>
2021-06-11 15:36:47 +0200

    Update

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,70 @@
+let sessionId;
+let submitted = false;
+let audioElem;
 
-
-function initClient(){
-
+function initClient(session){
+  sessionId = session;
+  $('#submitAnswerButton').prop('disabled', true);
+  $('#audioPauseIcon').hide();
+  $(window).on("beforeunload", () => {
+    if(!submitted) {
+      let host = window.location.protocol + "//" + window.location.host + "/close";
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", host, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(JSON.stringify({
+        session: sessionId,
+      }));
+    }
+  });
+  audioElem = $('#audioSource');
+  if(audioElem){
+    audioElem.on("ended", () => {
+      $('#audioPlayIcon').show();
+      $('#audioPauseIcon').hide();
+    });
+  }
 }
 
+function playPauseAudio(){
+  let audioElemSource = audioElem.get(0);
+  if(audioElemSource.paused) {
+    audioElemSource.play();
+    $('#audioPlayIcon').hide();
+    $('#audioPauseIcon').show();
+  }
+  else {
+    audioElemSource.pause();
+    $('#audioPlayIcon').show();
+    $('#audioPauseIcon').hide();
+  }
+}
 
+function onFormClick(){
+  let value = $('input[name="answerForm"]:checked').val();
+  if(value && value !== "" && !submitted) {
+    $('#submitAnswerButton').prop('disabled', false);
+  }
+}
 
 function submitAnswer(){
-  let inputElem = $('#usernameInput');
-  let newName = inputElem.val();
-  if(newName !== "") {
-    inputElem.prop( "disabled", true );
+  let value = $('input[name="answerForm"]:checked').val();
+  if(value && value !== "") {
+    $('#submitAnswerButton').prop('disabled', true);
+    submitted = true;
+
+    let host = window.location.protocol + "//" + window.location.host;
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        location.reload();
+      }
+    }
+    xhr.open("POST", host, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+      session: sessionId,
+      value: value
+    }));
   }
 }
--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const fs = require("fs");
 const {MongoClient} = require("mongodb");
 const httpPort = 3000;
 const httpsPort = 3443;
+const sessionCheckInterval = 3600000;
+const sessionTimeout = 3600000 * 4;
+const { v4: uuidv4 } = require('uuid');
+
+let sessionList = {};
 
 async function main(){
     const dbData = getDbData();
@@ -17,6 +22,7 @@ async function main(){
     const client = new MongoClient(uri, {"useUnifiedTopology": true});
 
     app.set("view engine", "pug")
+    app.use(express.json());
     app.use((req, res, next) => {
         if (req.secure) {
             next();
@@ -30,7 +36,8 @@ async function main(){
     app.use("/js", express.static(path.join(__dirname, "js/")));
     app.use("/controllers", express.static(path.join(__dirname, "controllers/")));
     app.use("/css", express.static(path.join(__dirname, "css/")));
-    app.use("/img", express.static(path.join(__dirname, "img/")));
+    app.use("/content", express.static(path.join(__dirname, "content/")));
+    app.use("/icons", express.static(path.join(__dirname, "icons/")));
 
     let privateKey  = fs.readFileSync(path.join(__dirname, "/private/selfsigned.key"));
     let certificate = fs.readFileSync(path.join(__dirname, "/private/selfsigned.crt"));
@@ -47,15 +54,42 @@ async function main(){
 
     try {
         await client.connect();
+        setInterval(checkSessionList, sessionCheckInterval);
 
         app.get("/", async(req, res, next) => {
             async function runAsync () {
-                // let question = "filler";
+                let sessionId = uuidv4();
                 let question = await getQuestion(client);
-                res.render("index", {question: question["question"], message: "Hello there!"});
+                let date = new Date();
+                sessionList[sessionId] = {
+                    "question": question,
+                    "time": date.getTime()
+                }
+                res.render("index", {
+                    question: question["question"],
+                    type: question["type"],
+                    content: question["content"],
+                    session: sessionId
+                });
             }
             runAsync()
                 .catch(next)
+        });
+
+        app.post("/", async(req, res, next) => {
+            async function runAsync () {
+                let sessionId = req.body["session"];
+                let answer = req.body["value"];
+                await putAnswer(client, sessionId, answer);
+                res.sendStatus(200);
+            }
+            runAsync()
+                .catch(next)
+        });
+
+        app.post("/close", (req, res) => {
+            let sessionId = req.body["session"];
+            delete sessionList[sessionId];
         });
 
         process.on("SIGINT", async () => {
@@ -70,31 +104,55 @@ async function main(){
 }
 
 async function getQuestion(client) {
-    let cursor = client.db().collection("answers").find();
+    const answersCollection = client.db().collection("answers");
+    const questionsCollection = client.db().collection("questions");
+
+    let cursor = answersCollection.find();
     let answerAmount = await cursor.count();
     if(answerAmount === 0){
-        let questions =  await client.db().collection("questions").find().toArray();
+        let questions =  await questionsCollection.find().toArray();
         return questions[Math.floor(Math.random() * questions.length)];
     }
     else {
-        let answeredQuestionsIds = await client.db().collection("answers").distinct("_id");
-        let unansweredQuestions =
-            await client.db().collection("questions").distinct("_id", {_id: {$nin: answeredQuestionsIds}});
+        let answeredQuestionsIds = await answersCollection.distinct("_id");
+        let unansweredQuestions = await questionsCollection.distinct("_id", {_id: {$nin: answeredQuestionsIds}});
         if(unansweredQuestions.length > 0){
             let randomIndex = Math.floor(Math.random() * unansweredQuestions.length);
             let toGetId = unansweredQuestions[randomIndex];
-            return await client.db().collection("questions").findOne({_id: toGetId});
+            return await questionsCollection.findOne({_id: toGetId});
         }
         else {
-            let idCount = await client.db().collection("answers").aggregate(
-                [{$group: {_id: "$_id", count: {$sum: 1}}}]
+            let idCount = await answersCollection.aggregate(
+                [{$group: {_id: "$qId", count: {$sum: 1}}}]
             ).toArray();
-            let min = Math.min(...idCount.map(item => item.count));
-            let randomIndex = Math.floor(Math.random() * idCount.length);
-            let toGetId = idCount.filter(item => item.count === min)[randomIndex]["_id"];
-            return await client.db().collection("questions").find({_id: toGetId});
+            let lowestRandomId = getRandomMin(idCount)["_id"];
+            return await questionsCollection.findOne({_id: lowestRandomId});
         }
     }
+}
+
+async function putAnswer(client, sessionId, answer){
+    const answers = client.db().collection("answers");
+    let question = sessionList[sessionId]["question"];
+    if(question) {
+        let toPutAnswer = {
+            "qId": question["_id"],
+            "qType": question["type"],
+            "result": answer
+        }
+        let result = await answers.insertOne(toPutAnswer);
+        delete sessionList[sessionId]
+        //console.log(result);
+    }
+    else {
+        console.log("Session not available!")
+    }
+}
+
+function getRandomMin(countObject){
+    let min = Math.min(...countObject.map(item => item.count));
+    let randomIndex = Math.floor(Math.random() * countObject.length);
+    return  countObject.filter(item => item.count === min)[randomIndex];
 }
 
 function getDbData(){
@@ -107,6 +165,23 @@ function getDbData(){
     return result
 }
 
-
+function checkSessionList(){
+    let toRemoveSessions = [];
+    for (let key in sessionList) {
+        let sessionValues = sessionList[key];
+        let creationTime = sessionValues["time"];
+        let date = new Date();
+        let existenceTime = date.getTime() - creationTime;
+        console.log("Existence time: " + existenceTime);
+        if(existenceTime > sessionTimeout){
+            console.log("Add session to remove: " + key);
+            toRemoveSessions.push(key);
+        }
+    }
+    for(let sessionId of toRemoveSessions){
+        console.log("Delete session: " + sessionId);
+        delete sessionList[sessionId]
+    }
+}
 
 main().catch(console.error);
